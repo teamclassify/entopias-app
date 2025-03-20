ddb34bff64ec84bef85b049a048576cb591268fc
Andres Parra <byandrev@gmail.com>
2025-03-19 20:25:35 -0500

    feat: setup wouter

---
+++ src/pages/signin/index.jsx
@@ -0,0 +1,9 @@
+function SignIn() {
+  return (
+    <>
+      <h1>Iniciar Sesion</h1>
+    </>
+  );
+}
+
+export default SignIn;
---
+++ src/pages/signup/index.jsx
@@ -0,0 +1,9 @@
+function SignUp() {
+  return (
+    <>
+      <h1>Registrarse</h1>
+    </>
+  );
+}
+
+export default SignUp;
