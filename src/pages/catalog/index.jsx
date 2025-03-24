dc7ecfd48ab4a99a86582076e82089454e5c3938
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-03-24 16:20:24 -0500

    feat: add slider component and price filter

--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,24 +1,24 @@
 import DefaultLayout from "@/components/layouts/DefaultLayout";
 import CardProduct from "./components/CardProduct";
-import Filter from "./components/Filter";
+import Filters from "./components/Filters";
 
 function Index() {
   return (
     <DefaultLayout>
-     <div className="w-full max-w-screen-lg mx-auto">
-  <div className="flex flex-col md:flex-row gap-8 pt-4">
-    <aside className="w-full md:w-1/4">
-      <Filter />
-    </aside>
-    <main className="w-full md:w-3/4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {[...Array(8)].map((_, i) => (
-          <CardProduct key={i} />
-        ))}
+      <div className="w-full max-w-screen-lg mx-auto">
+        <div className="flex flex-col md:flex-row gap-8 pt-4">
+          <aside className="w-full md:w-1/4">
+            <Filters />
+          </aside>
+          <main className="w-full md:w-3/4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {[...Array(8)].map((_, i) => (
+                <CardProduct key={i} />
+              ))}
+            </div>
+          </main>
+        </div>
       </div>
-    </main>
-  </div>
-</div>
     </DefaultLayout>
   );
 }
