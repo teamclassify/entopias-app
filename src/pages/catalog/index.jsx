bdc0fec57ea9f327b6542b8e2195503de111a5d5
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-03-24 12:29:16 -0500

    feat: add aspect-ratio component

--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,5 +1,21 @@
+import DefaultLayout from "@/components/layouts/DefaultLayout";
+import CardProduct from "./components/CardProduct";
+
 function Index() {
-  return <h1>Hola mundo</h1>;
+  return (
+    <DefaultLayout>
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4">
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+      </div>
+    </DefaultLayout>
+  );
 }
 
 export default Index;
