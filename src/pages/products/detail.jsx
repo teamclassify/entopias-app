27afa06f6788e1fd2a73360c3179fef9b9e8bd46
MauricioDDS <mauriciodidonato794@gmail.com>
2025-03-24 14:28:00 -0500

    fix: add coffee image to src/assets

--- src/pages/products/detail.jsx
+++ src/pages/products/detail.jsx
@@ -2,6 +2,8 @@ import { useRoute } from "wouter";
 import DefaultLayout from "@/components/layouts/DefaultLayout";
 import productsData from "@/data/products.json";
 
+import cafeImage from '@/assets/cafe.png'; 
+
 function ProductDetail() {
   const [match, params] = useRoute("/producto/:id");
 
@@ -14,6 +16,7 @@ function ProductDetail() {
 
   return (
     <DefaultLayout>
+      <img src={cafeImage} alt={product.name} width={300} />
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p><strong>Precio:</strong> {product.price}</p>
