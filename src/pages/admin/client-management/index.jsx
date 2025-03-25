01245950741289447ec3789876da7eeca43fdabd
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-03-25 18:30:29 -0500

    feat: add input filter

--- src/pages/admin/client-management/index.jsx
+++ src/pages/admin/client-management/index.jsx
@@ -3,12 +3,18 @@ import { columns } from "../../../components/tables/Columns";
 import { data } from "../../../mocks/clients.json";
 import DataTable from "../../../components/tables/DataTable";
 import PaginationComponent from "../../catalog/components/Pagination";
+import { Input } from "@/components/ui/input.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 function ClientManagment() {
   return (
     <AdminLayout>
       <main>
         <h1 className="font-bold">Clientes</h1>
+        <section className="flex flex-row gap-3 pt-4 pb-4">
+          <Input placeholder="Buscar" />
+          <Button variant="outline">Buscar</Button>
+        </section>
         <div className="pt-3">
           <DataTable columns={columns} data={data || []} />
         </div>
