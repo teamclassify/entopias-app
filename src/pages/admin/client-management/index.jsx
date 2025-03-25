bf8531256e13198b26f293975e48b859b9c42e74
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-03-24 19:18:14 -0500

    feat: create branch

---
+++ src/pages/admin/client-management/index.jsx
@@ -0,0 +1,10 @@
+import AdminLayout from "../../../components/layouts/AdminLayout";
+
+function ClientManagment() {
+  return (
+    <AdminLayout>
+      <h1>Holaa</h1>
+    </AdminLayout>
+  );
+}
+export default ClientManagment;
