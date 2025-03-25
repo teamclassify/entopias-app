a1f2ea0ea9119d8857ebcf54245235e4397a5d24
Andres Parra <byandrev@gmail.com>
2025-03-25 16:29:17 -0500

    chore: add Supabase client configuration and file upload functionality

---
+++ src/config/supabase.js
@@ -0,0 +1,28 @@
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_KEY, SUPABASE_URL } from ".";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+/*
+ * Uploads a file to the specified bucket.
+ * @param {string} bucket - The name of the bucket.
+ * @param {File} file - The file to upload.
+ * @param {string} name - The name of the file.
+ * @returns {Promise} - The result of the upload.
+ */
+async function uploadFile(bucket, file, name) {
+  const { data, error } = await supabase.storage
+    .from(bucket)
+    .upload(`public/${name}`, file);
+
+  if (error) {
+    console.error("Error uploading file: ", error);
+    return error;
+  }
+
+  console.log("File uploaded: ", data);
+
+  return data;
+}
+
+export { supabase, uploadFile };
