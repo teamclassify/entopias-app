bb06a8ce04bef3e1303b6e0b7611800f1f278c15
Andres Parra <byandrev@gmail.com>
2025-03-26 19:39:01 -0500

    feat: make phone number optional in sales account form validation

--- src/pages/sales/components/Form.jsx
+++ src/pages/sales/components/Form.jsx
@@ -18,7 +18,7 @@ const formSchema = z.object({
   name: z.string().min(3).max(255),
   email: z.string().email(),
   password: z.string().min(6),
-  phone: z.string().min(10),
+  phone: z.string().min(10).optional(),
 });
 
 /*
