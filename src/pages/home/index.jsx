1ee626a496af8cbd8bd99ef6bda1eefbb47b1cd9
luisalealr <luisafernandalero@gmail.com>
2025-03-26 23:58:58 -0500

    Chance: Homepage ending

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -84,7 +84,7 @@ function HomePage() {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-10 p-6 border-t border-gray-300 mb-30">
+      <div className="grid grid-cols-3 gap-10 p-6 border-t border-gray-300 mb-40">
         {features.map((feature, index) => (
           <div className="flex flex-row items-center">
             <div key={index} className="flex flex-col items-center text-center">
@@ -98,7 +98,28 @@ function HomePage() {
           </div>
         ))}
       </div>
-      
+      <div className="relative flex justify-center items-center py-20 my-30">
+        {/* Imagen */}
+        <div className="absolute left-[30%] top-[40%] translate-x-[-60%] translate-y-[-40%] w-[470px] z-10">
+          <img
+            src="/taza-de-cafe.jpg"
+            alt="Café"
+            className="w-full shadow-2xl"
+          />
+        </div>
+        {/* Cuadro */}
+        <div className="absolute bg-[#9C5B40] text-white p-6 w-[600px] h-[300px] shadow-lg 
+                     left-[50%] top-[50%] translate-x-[-18%] translate-y-[-60%]">
+          <div className="pl-20 flex flex-col justify-center items-center h-full">
+            <p className="text-lg">
+              Nuestro café especial es un viaje sensorial que despierta los sentidos con sabores y aromas únicos.
+            </p>
+            <p className="mt-10 text-lg">
+              Cada sorbo es un abrazo matutino que ilumina el día con notas dulces y tostadas, creando un ritual reconfortante que llena de energía y alegría desde el primer aroma hasta el último sorbo.
+            </p>
+          </div>
+        </div>
+      </div>
     </DefaultLayout>
   );
 }
