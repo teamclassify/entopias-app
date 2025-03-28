1bab4d6fdea5cbb4f57b1ba495f533053798ae96
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-03-28 16:36:19 -0500

    feat: change image height,  90svh instead of px

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -15,7 +15,8 @@ function HomePage() {
     {
       icon: "👥",
       title: "Atención al cliente",
-      description: "Contamos con una alta calidad de servicio a nuestros clientes",
+      description:
+        "Contamos con una alta calidad de servicio a nuestros clientes",
     },
     {
       icon: "🚚",
@@ -31,22 +32,35 @@ function HomePage() {
 
   return (
     <DefaultLayout>
-      <div className="h-[650px] w-screen relative left-1/2 -translate-x-1/2 bg-cover bg-center -mt-8 flex justify-center items-center mb-30"
-        style={{ backgroundImage: `url("/imagen-cafe-fondo.jpg")` }}>
-          <div className="absolute inset-0 bg-[#b76e4944]"></div>
+      <div className="w-screen relative left-1/2 -translate-x-1/2 bg-cover bg-center -mt-8 flex justify-center items-center mb-30">
+        <div className="absolute inset-0 h-[90svh]">
+          <img
+            className="h-[90svh] w-full object-cover"
+            src="/imagen-cafe-fondo.jpg"
+          />
+          <div className="absolute inset-0 bg-[#b76e49] opacity-[.27]"></div>
+        </div>
         <div className="text-white w-[80%] relative">
           <div className="flex flex-row justify-between w-full h-[80%]">
             <div className="w-[50%]">
-              <img className="h-[600px] w-auto" src="/cafes-inicio.png" alt="" />
+              <img
+                className="h-[600px] w-auto"
+                src="/cafes-inicio.png"
+                alt=""
+              />
             </div>
             <div className="flex flex-col h-[500px] w-[50%] justify-center items-start">
-              <div  >
+              <div>
                 <div>
-                  <h1 className="text-5xl font-bold" >Un café premium para momento únicos</h1>
+                  <h1 className="text-5xl font-bold">
+                    Un café premium para momento únicos
+                  </h1>
                 </div>
                 <hr className="my-7 " />
                 <div>
-                  <h3 className="text-2xl w-[80%] font-medium">Saborea experiencias inolvidables con cada taza</h3>
+                  <h3 className="text-2xl w-[80%] font-medium">
+                    Saborea experiencias inolvidables con cada taza
+                  </h3>
                 </div>
               </div>
             </div>
@@ -59,18 +73,20 @@ function HomePage() {
         </div>
         <div className="flex flex-col w-[60%] items-start pl-16">
           <div>
-            <h2 className="text-2xl font-bold mb-2" >Sobre Nosotros</h2>
+            <h2 className="text-2xl font-bold mb-2">Sobre Nosotros</h2>
           </div>
           <div>
             <p className="text-[16px] mb-4">
-              En Entopias Café, vivimos la pasión por el buen café. Seleccionamos granos de alta calidad directamente
-              de productores, asegurando autenticidad y sabor en cada taza. Nos enfocamos en resaltar las mejores
-              características de cada cosecha a través de un proceso cuidadoso que garantiza frescura y excelencia.
-
+              En Entopias Café, vivimos la pasión por el buen café.
+              Seleccionamos granos de alta calidad directamente de productores,
+              asegurando autenticidad y sabor en cada taza. Nos enfocamos en
+              resaltar las mejores características de cada cosecha a través de
+              un proceso cuidadoso que garantiza frescura y excelencia.
             </p>
             <p className="text-[16px]">
-              Más que una marca, somos una comunidad que celebra la tradición cafetera, conectando a los amantes del
-              café con experiencias únicas y auténticas.
+              Más que una marca, somos una comunidad que celebra la tradición
+              cafetera, conectando a los amantes del café con experiencias
+              únicas y auténticas.
             </p>
           </div>
         </div>
@@ -90,11 +106,15 @@ function HomePage() {
           <div className="flex flex-row items-center">
             <div key={index} className="flex flex-col items-center text-center">
               <div className="text-4xl">{feature.icon}</div>
-              <h3 className="font-semibold text-lg text-[#9C5B40] mt-2">{feature.title}</h3>
+              <h3 className="font-semibold text-lg text-[#9C5B40] mt-2">
+                {feature.title}
+              </h3>
               <p className="text-gray-600 mt-1">{feature.description}</p>
             </div>
             <div>
-              {index < features.length - 1 && <div className="hidden md:block border-l border-gray-300 h-36 mx-6"></div>}
+              {index < features.length - 1 && (
+                <div className="hidden md:block border-l border-gray-300 h-36 mx-6"></div>
+              )}
             </div>
           </div>
         ))}
@@ -109,14 +129,19 @@ function HomePage() {
           />
         </div>
         {/* Cuadro */}
-        <div className="absolute bg-[#9C5B40] text-white p-6 w-[600px] h-[300px] shadow-lg 
-                     left-[50%] top-[50%] translate-x-[-18%] translate-y-[-60%]">
+        <div
+          className="absolute bg-[#9C5B40] text-white p-6 w-[600px] h-[300px] shadow-lg 
+                     left-[50%] top-[50%] translate-x-[-18%] translate-y-[-60%]"
+        >
           <div className="pl-20 flex flex-col justify-center items-center h-full">
             <p className="text-lg">
-              Nuestro café especial es un viaje sensorial que despierta los sentidos con sabores y aromas únicos.
+              Nuestro café especial es un viaje sensorial que despierta los
+              sentidos con sabores y aromas únicos.
             </p>
             <p className="mt-10 text-lg">
-              Cada sorbo es un abrazo matutino que ilumina el día con notas dulces y tostadas, creando un ritual reconfortante que llena de energía y alegría desde el primer aroma hasta el último sorbo.
+              Cada sorbo es un abrazo matutino que ilumina el día con notas
+              dulces y tostadas, creando un ritual reconfortante que llena de
+              energía y alegría desde el primer aroma hasta el último sorbo.
             </p>
           </div>
         </div>
