5920a4e9bf470c42223954204181c79505ae08f7
Andres Parra <byandrev@gmail.com>
2025-03-31 18:27:25 -0500

    feat: add CheckboxRole component for managing user sales role

---
+++ src/pages/sales/components/CheckboxRole.jsx
@@ -0,0 +1,33 @@
+import { Checkbox } from "@/components/ui/checkbox";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import UsersService from "../../../services/api/Users";
+
+function CheckboxRole({ userId, roles }) {
+  const [isSales, setIsSales] = useState(
+    roles.some((role) => role.roleId === 1)
+  );
+
+  const { mutate } = useMutation({
+    mutationFn: (data) => {
+      return UsersService.updateRole(data);
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      setIsSales(!data.data.error);
+    },
+  });
+
+  const handleChange = (checked) => {
+    setIsSales(checked);
+
+    mutate({
+      id: userId,
+      role: checked ? "sales" : null,
+    });
+  };
+
+  return <Checkbox defaultChecked={isSales} onCheckedChange={handleChange} />;
+}
+
+export default CheckboxRole;
