44e562a27a015dabdeb094ba079711042c1e9017
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-04-02 18:32:12 -0500

    feat: use data.json for products, but it's error in api

--- src/pages/catalog/components/CardProduct.jsx
+++ src/pages/catalog/components/CardProduct.jsx
@@ -1,25 +1,22 @@
 import { Link } from "wouter";
-import { IoHeartOutline } from "react-icons/io5";
-import { TiShoppingCart } from "react-icons/ti";
-
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 
-function CardProduct() {
+function CardProduct({ infoProduct }) {
   return (
-    <Link href={"/producto/1"}>
+    <Link href={`/producto/${infoProduct.id}`}>
       <div className="card">
         <AspectRatio ratio={3 / 4}>
           <div className="bg-[#ECECEC] h-full w-full flex justify-center items-center">
             <img
-              src="/cafe.webp"
+              src={infoProduct.photos[0].url}
               className="card-img-top object-cover h-[95%]"
               alt={"imagen de café entopias"}
             />
           </div>
         </AspectRatio>
         <div className="text-center border-2 border-[#ECECEC]">
-          <h5 className="text-md">Cafe especial</h5>
-          <p className="text-sm font-bold text-[#F03634]">$40.000</p>
+          <h5 className="text-md">{infoProduct.name}</h5>
+          <p className="text-sm font-bold text-[#F03634]">${infoProduct.precio.toLocaleString("es-CO")}</p>
         </div>
       </div>
     </Link>
--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,12 +1,31 @@
 import DefaultLayout from "@/components/layouts/DefaultLayout";
 import CardProduct from "./components/CardProduct";
 import Filters from "./components/Filters";
-import PaginationComponent from "./components/Pagination";
+import ProductsService from "../../services/api/Products";
+import { Skeleton } from "@/components/ui/skeleton";
+//import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Pagination from "./components/Pagination";
+import { Loading } from "@/components/ui/loading";
+import {data} from "@/mocks/data.json"
 
 function Index() {
+  //const [page, setPage] = useState(1);
+
+  /*
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["products", page],
+    queryFn: () => ProductsService.getAll({ page }),
+  });
+
+  if (isError || data?.error) {
+    return <Error message={data?.msg} />;
+  }
+
+  */
   return (
     <DefaultLayout>
-      <div className="w-full max-w-screen-lg mx-auto"> 
+      <div className="w-full max-w-screen-lg mx-auto">
         <div className="flex flex-col md:flex-row gap-8 pt-4">
           <aside className="w-full md:w-1/4 mr-5">
             <Filters />
@@ -16,13 +35,27 @@ function Index() {
               <p className="font-bold pb-3">Mostrando 8-12 de 15 resultados</p>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-              {[...Array(8)].map((_, i) => (
-                <CardProduct key={i} />
-              ))}
+            {/*}
+              {isLoading ? (
+                <Loading />
+              ) : ({*/
+                data.products.map((infoProduct, i) => (
+                  <CardProduct key={i} infoProduct={infoProduct} />
+                ))
+            }
             </div>
           </main>
         </div>
-        <PaginationComponent />
+        {/*}
+        <Pagination
+          currentPage={page}
+          totalItems={data.data.count || 0}
+          itemsPerPage={10}
+          onPageChange={(page) => {
+            setPage(page);
+          }}
+        />
+        {*/}
       </div>
     </DefaultLayout>
   );
