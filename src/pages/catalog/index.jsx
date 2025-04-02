da2ef96d7b010e7efab9e22a04d02feed66d75a6
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-04-02 18:36:32 -0500

    test: add data from db

--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -2,17 +2,16 @@ import DefaultLayout from "@/components/layouts/DefaultLayout";
 import CardProduct from "./components/CardProduct";
 import Filters from "./components/Filters";
 import ProductsService from "../../services/api/Products";
-import { Skeleton } from "@/components/ui/skeleton";
-//import { useQuery } from "@tanstack/react-query";
+//import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import Pagination from "./components/Pagination";
 import { Loading } from "@/components/ui/loading";
-import {data} from "@/mocks/data.json"
+//import {data} from "@/mocks/data.json"
 
 function Index() {
-  //const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
-  /*
   const { data, isLoading, isError } = useQuery({
     queryKey: ["products", page],
     queryFn: () => ProductsService.getAll({ page }),
@@ -22,7 +21,6 @@ function Index() {
     return <Error message={data?.msg} />;
   }
 
-  */
   return (
     <DefaultLayout>
       <div className="w-full max-w-screen-lg mx-auto">
@@ -35,18 +33,16 @@ function Index() {
               <p className="font-bold pb-3">Mostrando 8-12 de 15 resultados</p>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-            {/*}
               {isLoading ? (
                 <Loading />
-              ) : ({*/
+              ) : (
                 data.products.map((infoProduct, i) => (
                   <CardProduct key={i} infoProduct={infoProduct} />
                 ))
-            }
+              )}
             </div>
           </main>
         </div>
-        {/*}
         <Pagination
           currentPage={page}
           totalItems={data.data.count || 0}
@@ -55,7 +51,6 @@ function Index() {
             setPage(page);
           }}
         />
-        {*/}
       </div>
     </DefaultLayout>
   );
