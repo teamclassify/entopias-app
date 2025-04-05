401edc4d29b779489fe41f3ebc7a18f6dd700488
Andres Parra <byandrev@gmail.com>
2025-04-05 17:18:20 -0500

    feat: integrate i18next for internationalization in HeroSection and LogoInfo components

--- src/pages/home/components/LogoInfo.jsx
+++ src/pages/home/components/LogoInfo.jsx
@@ -1,4 +1,8 @@
+import { useTranslation } from "react-i18next";
+
 function LogoInfo() {
+  const { t } = useTranslation();
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-around gap-4 mb-30 w-full pt-5">
       <img
@@ -7,17 +11,10 @@ function LogoInfo() {
         alt="logo de la empresa entopias cafe"
       />
       <div className="flex flex-col items-center sm:items-baseline sm:w-[60%] w-full">
-        <h2 className="text-2xl font-bold mb-2">Sobre Nosotros</h2>
-        <p className="text-[16px] mb-4">
-          En Entopias Café, vivimos la pasión por el buen café. Seleccionamos
-          granos de alta calidad directamente de productores, asegurando
-          autenticidad y sabor en cada taza. Nos enfocamos en resaltar las
-          mejores características de cada cosecha a través de un proceso
-          cuidadoso que garantiza frescura y excelencia. <br />
-          <br /> Más que una marca, somos una comunidad que celebra la tradición
-          cafetera, conectando a los amantes del café con experiencias únicas y
-          auténticas.
-        </p>
+        <h2 className="text-2xl font-bold mb-2">
+          {t("homePage.about_us.title")}
+        </h2>
+        <p className="text-[16px] mb-4">{t("homePage.about_us.description")}</p>
       </div>
     </div>
   );
