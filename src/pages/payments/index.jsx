9f77466e0dba1167eb1dc37da32cffc4291c19b7
Andres Parra <byandrev@gmail.com>
2025-04-19 19:56:27 -0500

    feat: add Payments page with CheckoutForm component for payment processing

---
+++ src/pages/payments/index.jsx
@@ -0,0 +1,11 @@
+import CheckoutForm from "@/components/payments/CheckoutForm";
+
+function Payments() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <CheckoutForm />
+    </div>
+  );
+}
+
+export default Payments;
