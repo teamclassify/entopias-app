ea7a9bc2c1a3473250d197d8933cc228f486849b
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-04-21 18:36:04 -0500

    feat: add cart services

---
+++ src/services/api/Cart.js
@@ -0,0 +1,119 @@
+import axios from "axios";
+import { URL, handleAxiosError } from ".";
+import { getToken } from "./Auth";
+
+async function getAll() {
+  const token = await getToken();
+
+  if (!token) throw new Error("Token not found");
+
+  try {
+    const res = await axios({
+      url: `${URL}/cart`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    handleAxiosError(error);
+  }
+}
+
+async function create(data) {
+  const token = await getToken();
+
+  if (!token) throw new Error("Token not found");
+
+  try {
+    const res = await axios({
+      url: `${URL}/cart`,
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    });
+    return res.data;
+  } catch (error) {
+    handleAxiosError(error);
+  }
+}
+
+async function add(data) {
+  try {
+    const token = await getToken();
+
+    if (!token) throw new Error("Token not found");
+
+    const res = await axios({
+      url: `${URL}/cart`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        varietyId: data.varietyId,
+        quantity: data.quantity,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    return handleAxiosError(error);
+  }
+}
+
+async function update( data ) {
+  try {
+    const token = await getToken();
+
+    if (!token) throw new Error("Token not found");
+
+    const res = await axios({
+      url: `${URL}/cart`,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        varietyId: data.varietyId,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    return handleAxiosError(error);
+  }
+}
+
+async function remove(data) {
+  const token = await getToken();
+
+  if (!token) throw new Error("Token not found");
+
+  try {
+    const res = await axios({
+      url: `${URL}/cart`,
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        varietyId: data.varietyId,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    handleAxiosError(error);
+  }
+}
+const CartServices = { getAll, create, add, remove, update };
+
+export default CartServices;
