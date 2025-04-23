0df0d9bf1074819f2426d432d0f2860dd2d5b0f1
Andres Parra <byandrev@gmail.com>
2025-04-23 18:44:50 -0500

    feat: add delete and update functions to AddressService for address management

--- src/services/api/Address.js
+++ src/services/api/Address.js
@@ -46,9 +46,55 @@ async function createAddress({ city, country, postalCode, address }) {
   }
 }
 
+async function deleteAddress(id) {
+  const token = await getToken();
+
+  try {
+    const res = await axios({
+      url: `${URL}/addresses/${id}`,
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    return handleAxiosError(error);
+  }
+}
+
+async function updateAddress(id, { city, country, postalCode, address }) {
+  const token = await getToken();
+
+  try {
+    const res = await axios({
+      url: `${URL}/addresses/${id}`,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        city,
+        country,
+        postalCode,
+        address,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    return handleAxiosError(error);
+  }
+}
+
 const AddressService = {
   createAddress,
   getAddress,
+  deleteAddress,
+  updateAddress,
 };
 
 export default AddressService;
