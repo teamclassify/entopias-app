cd81fe31348b01a87ad1341d4e25fa3074680db0
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-04-23 21:55:21 -0500

    refactor: delete image default

--- src/pages/cart/components/ProductCartInfo.jsx
+++ src/pages/cart/components/ProductCartInfo.jsx
@@ -3,12 +3,11 @@ import Quantity from "./Quantity";
 
 export default function ProductCartInfo({ product, quantity, setQuantity }) {
   const coffeInfo = getProductInfoConfig(product);
-  const defaultImage = "/cafe.webp";
+  //const defaultImage = "/cafe.webp";
 
   return (
     <div className="flex">
-      {/* <img src={product.variety.product.photos[0].url} className="w-5/12 m-4 rounded-2xl" /> */}
-      <img src={defaultImage} className="w-5/12 m-4 rounded-2xl" />
+      <img src={product.variety.product.photos[0].url} className="w-5/12 m-4 rounded-2xl" />
       <div className="flex flex-col pt-5 gap-2">
         {coffeInfo.map((item, index) => (
           <p key={index} className={item.className || ""}>
