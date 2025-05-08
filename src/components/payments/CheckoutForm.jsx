b24eab9072e0f88ca3291732bd2d0cf50ba8efa4
Andres Parra <byandrev@gmail.com>
2025-05-08 08:49:25 -0500

    refactor: integrate localization for currency selection in CheckoutForm

--- src/components/payments/CheckoutForm.jsx
+++ src/components/payments/CheckoutForm.jsx
@@ -5,12 +5,15 @@ import {
 import { loadStripe } from "@stripe/stripe-js";
 import { useCallback } from "react";
 
+import { useTranslation } from "react-i18next";
 import { STRIPE_PUBLIC_KEY } from "../../config";
 import paymentsService from "../../services/api/Payments";
 
 const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
 
 function CheckoutForm() {
+  const { i18n } = useTranslation();
+
   const fetchClientSecret = useCallback(async () => {
     const selectedAddress = localStorage.getItem("selectedAddress");
 
@@ -22,11 +25,11 @@ function CheckoutForm() {
     const selectedAddressId = JSON.parse(selectedAddress).id;
 
     const res = await paymentsService.createPaymentIntent({
-      currency: "cop",
+      currency: i18n.language === "en" ? "usd" : "cop",
       address: selectedAddressId,
     });
     return res.data.session.client_secret;
-  }, []);
+  }, [i18n.language]);
 
   const options = { fetchClientSecret };
 
