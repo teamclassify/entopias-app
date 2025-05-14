284bc108f6ebcaad130d8de2169e541f023470b0
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-05-13 23:37:15 -0500

    feat: advance in fix footer

--- src/components/layouts/DefaultLayout.jsx
+++ src/components/layouts/DefaultLayout.jsx
@@ -5,12 +5,10 @@ function DefaultLayout({ children, className = "" }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header className="w-[90%] max-w-screen-lg mx-auto" />
-
-      <main className="w-[90%] py-8 mx-auto max-w-screen-lg flex-grow">
-        <div className={`${className}`}>{children}</div>
+      <main className="flex-grow w-[90%] py-8 mx-auto max-w-screen-lg">
+        <div className={className}>{children}</div>
       </main>
-
-      <Footer className="w-[90%] max-w-screen-lg mx-auto" />
+      <Footer className="w-full" />
     </div>
   );
 }
