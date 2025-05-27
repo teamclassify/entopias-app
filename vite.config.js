b28fe15f64248ab5b8a2c5d27a166c072a73bf69
Andres Parra <120739623+byandrev@users.noreply.github.com>
2025-05-27 11:05:58 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,14 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig, loadEnv } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return defineConfig({
-     plugins: [react(), tailwindcss()],
-resolve: {
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
-    server: {
-      port: parseInt(env.VITE_PORT),
-      host: true,
-      cors: true,
-      allowedHosts: [`${VITE_FIREPLOY_HOST}`],
     },
-    preview: {
-      port: parseInt(env.VITE_PORT),
-      host: true, // permite 0.0.0.0
-      cors: true,
-      allowedHosts: [`${VITE_FIREPLOY_HOST}`],
-    },
-  });
-};
+  },
+});
