b3e4a415537ab83005a0e01e35d5e2420f850c53
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-05-28 11:50:46 -0500

    feat: create branch

---
+++ src/pages/stadistic/index.jsx
@@ -0,0 +1,10 @@
+export default function Stadistic() {
+  return (
+    <DefaultLayout
+      title="Estadísticas"
+      description="Estadísticas de ventas y productos"
+    >
+      <h1>Hello World</h1>
+    </DefaultLayout>
+  );
+}
