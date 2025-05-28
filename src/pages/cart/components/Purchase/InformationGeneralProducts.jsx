3b4c54aa48a56bac7b62b796782c2121b04d8f85
Andres Parra <byandrev@gmail.com>
2025-05-28 18:54:21 -0500

    feat: implement shipment service and integrate shipping cost calculation in address selection

--- src/pages/cart/components/Purchase/InformationGeneralProducts.jsx
+++ src/pages/cart/components/Purchase/InformationGeneralProducts.jsx
@@ -2,7 +2,7 @@ export default function InformationGeneralProducts({ totalPrice }) {
   return (
     <div className="flex justify-between font-semibold">
       Total
-      <p>${new Intl.NumberFormat().format(totalPrice || 0)}</p>
+      <p>${totalPrice || 0}</p>
     </div>
   );
 }
--- src/pages/cart/components/Purchase/PurchaseInfo.jsx
+++ src/pages/cart/components/Purchase/PurchaseInfo.jsx
@@ -3,17 +3,25 @@ import { formatPrice } from "../../../../utils/formatPrice";
 import InformationGeneralProducts from "./InformationGeneralProducts";
 import InformationPurchase from "./InformationPurchase";
 
-export default function PurchaseInfo({ lengthProducts, totalPrice }) {
+export default function PurchaseInfo({
+  lengthProducts,
+  totalPrice,
+  shipmentPrice,
+}) {
   const { t } = useTranslation();
 
   const price = formatPrice(totalPrice);
+  const shipping = formatPrice(shipmentPrice || 0);
   return (
     <div>
       <InformationPurchase name={t("cart.subtotal")} value={price} />
-      <InformationPurchase name={t("cart.shipping")} value={"$0.00"} />
+      <InformationPurchase
+        name={t("cart.shipping")}
+        value={shipping ?? "$0.00"}
+      />
       <InformationGeneralProducts
         lengthProducts={lengthProducts}
-        totalPrice={totalPrice}
+        totalPrice={Number(totalPrice) + Number(shipmentPrice) || 0}
       />
     </div>
   );
