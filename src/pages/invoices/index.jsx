5b9245c15732111647a329b734ba49557fca6103
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-05-28 22:48:13 -0500

    feat: show persistent loading toast while generating and downloading invoice PDF

--- src/pages/invoices/index.jsx
+++ src/pages/invoices/index.jsx
@@ -2,60 +2,56 @@ import { Button } from "@/components/ui/button";
 import InvoicesService from "../../services/api/Invoices";
 import AdminLayout from "../../components/layouts/AdminLayout";
 import ListOfSales from "./components/ListOfSales";
+import { File } from "lucide-react";
+import { toast } from "sonner";
 
 function InvoicesManagment() {
   // const [searchByName, setSearchByName] = useState("");
   // const [text] = useDebounce(searchByName, 500);
 
   const handleDownloadPdf = async () => {
-  try {
-    const blob = await InvoicesService.generatePdf({});
-
-    const url = window.URL.createObjectURL(new Blob([blob]));
-    const link = document.createElement("a");
-    link.href = url;
-    link.setAttribute("download", "reporte_facturas.pdf"); // 👈 nombre del archivo
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-    window.URL.revokeObjectURL(url); // liberar memoria
-  } catch (error) {
-    console.error("Error al generar el PDF:", error);
-  }
-};
-
+    try {
+      const toastID = toast.loading("Generando PDF...", {
+        duration: 0, // Indefinido hasta que se complete la acción
+      });
+      const blob = await InvoicesService.generatePdf({});
+      const url = window.URL.createObjectURL(new Blob([blob]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "reporte_facturas.pdf"); // 👈 nombre del archivo
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(url); // liberar memoria
+      toast.success("PDF generado exitosamente", {
+        id: toastID,
+      });
+    } catch (error) {
+      console.error("Error al generar el PDF:", error);
+    }
+  };
 
   return (
     <AdminLayout>
       <main>
-        <h1 className="text-2xl font-bold pt-6">Facturas</h1>
-        <div className="flex flex-row gap-2 ">
-          <Button
-            type="button"
-            variant="secondary"
-            onClick={handleDownloadPdf}
-            //onClick={() => {}}
-            className="mb-2"
-          >
-            PDF
-          </Button>
-          <Button
-            type="button"
-            variant="secondary"
-            //onClick={() => {}}
-            className="mb-2"
-          >
-            CSV
-          </Button>
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold pt-6">Facturas</h1>
+          <div className="flex justify-end gap-2 mt-6">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={handleDownloadPdf}
+              className="mb-2"
+            >
+              PDF
+              <File />
+            </Button>
+            {/* <Button type="button" className="mb-2">
+              CSV
+              <File />
+            </Button> */}
+          </div>
         </div>
-        {/* <section className="flex flex-row gap-3 pt-4 pb-4">
-          <Input
-            placeholder="Buscar"
-            value={searchByName}
-            onChange={(e) => setSearchByName(e.target.value)}
-          />
-          <Button variant="outline">Buscar</Button>
-        </section> */}
         <ListOfSales />
       </main>
     </AdminLayout>
