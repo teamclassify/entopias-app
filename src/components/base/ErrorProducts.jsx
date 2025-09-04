26f3c90ac04dc8d9bc94e06d7efa07121a9ad38d
Alessandro Daniele <alessandrodaniele07@gmail.com>
2025-09-04 15:00:19 -0500

    feat: add support english language in new component product available

--- src/components/base/ErrorProducts.jsx
+++ src/components/base/ErrorProducts.jsx
@@ -1,12 +1,15 @@
 import { Search } from "lucide-react"
+import { useTranslation } from "react-i18next";
 
 function ErrorProduct(){
+  const { t }= useTranslation();
+
   return (
   <div className="flex flex-col items-center justify-center text-center py-20">
         <Search size={64} className="text-gray-400 mb-4" />
-        <h2 className="text-xl font-semibold mb-2">No hay productos por ahora disponible</h2>
+        <h2 className="text-xl font-semibold mb-2">{t("errorProduct.title")}</h2>
         <p className="text-gray-500 max-w-md">
-          ¡Vuelve pronto! Estamos trabajando para ofrecerte los mejores productos.
+        {t("errorProduct.description")}
         </p>
       </div>
 )
